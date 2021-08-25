a2ede1bb3c3be5086c4b4a4a087cc900467a9d22
rcarmen <rcarmen@student.21-school.ru>
2021-08-26 02:56:35 +0300

    add: create_promp and freelst

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rcarmen <rcarmen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 21:26:17 by rcarmen           #+#    #+#             */
-/*   Updated: 2021/08/24 18:20:06 by rcarmen          ###   ########.fr       */
+/*   Updated: 2021/08/24 20:42: by rcarmen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,37 +124,103 @@ void	print_pipelinelst(t_lst *pipelinelst)
 	}
 }
 
+void init_shell()
+{
+	printf("\033[2J");
+	int fd;
+	char *line;
+	fd = open("greeting_art.txt", O_RDONLY);
+	if (fd != -1)
+	{
+		while (get_next_line(fd, &line) > 0)
+		{
+			printf("%s\n", line);
+			free(line);
+		}
+		printf("%s", line);
+		free(line);
+	}
+    printf("\n______________________________________________\n");
+    sleep(1);
+}
+
+char	*create_promp(char **line)
+{
+	char		*promp[2];
+
+	promp[0] = ft_strjoin(getenv("USER"), "\e[95m@\033[0m");
+	promp[1] = ft_strjoin(promp[0], getenv("HOME"));
+	free(promp[0]);
+	promp[0] = ft_strjoin(promp[1], "\033[0;32m> \033[0m");
+	free(promp[1]);
+	promp[1] = ft_strjoin("\033[0;32m", promp[0]);
+	*line = readline(promp[1]);
+	free(promp[0]);
+	free(promp[1]);
+	if (*line == NULL) // ctrl-d| позже допишу
+	{
+		printf(*line);
+		exit(1);
+	}
+}
+
+void	freelst(t_lst *tokenlst, t_lst *pipelinelst)
+{
+	t_lst *lsttmp;
+
+	while (tokenlst)
+	{
+		lsttmp = tokenlst->next;
+		free(tokenlst->value);
+		free(tokenlst);
+		tokenlst = lsttmp;;
+	}
+	while (pipelinelst)
+	{	
+		int	i = 0;
+		lsttmp = pipelinelst->next;
+		while (pipelinelst->type == TOKEN_CMD_ARGS && pipelinelst->cmd[i])
+		{
+			// printf("%s ", pipelinelst->cmd[i]);
+			free(pipelinelst->cmd[i]);
+			i++;
+		}
+		if (pipelinelst)
+			free(pipelinelst->cmd);
+		if (pipelinelst->type != TOKEN_CMD_ARGS)
+			free(pipelinelst->value);
+			// printf("\n%s\n", pipelinelst->value);
+		free(pipelinelst);
+		pipelinelst = lsttmp;
+	}
+}
+
 int	main(int ac, char **av, char **ep)
 {
 	char		*line;
 	t_lst		*tokenlst;
 	t_lst		*pipelinelst;
-	char		*promp;
 	t_lst		*pipelinelst_tmp;
 
-	// if (ac > 1 && av)
 	if (ac > 1)
 	{
 		printf("Error message: too many arguments!\n"); // TODO: временно, надо заменить на соотвуствующую ошибку.
 		return(1);
 	}
-	printf("%d\n", ac);
-	pipelinelst_tmp = pipelinelst;
-	promp = "\033[0;32mchillyshell\033[0;39m$ \033[0m";
+	init_shell();
+	// promp = "\033[0;32mchillyshell\033[0;39m$ \033[0m";
 	while (1)
 	{
+		in_signals();
 		tokenlst = NULL;
 		pipelinelst = NULL;
-		in_signals();
-		line = readline(promp);
-		if (line == NULL) // ctrl-d| позже допишу
-			exit(1);
+		create_promp(&line);
 		add_history(line);
 		get_tokenlst(line, &tokenlst);
 		get_pipelinelst(tokenlst, &pipelinelst);
 		// print_pipelinelst(pipelinelst);
-
 		// execute(pipelinelst);//пока не работает
-
+		free(line);
+		freelst(tokenlst, pipelinelst);
 	}	
 }
